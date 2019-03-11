5a78ff326b212e76bb08b062d1b2b47fb91f64c8
Alberto Duarte <betodute@gmail.com>
2019-03-11 20:02:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function add(x, y) {
+  return x + y ;
+}
+
+function sub(x, y) {
+  return x - y ;
+}
+
+function mult(x, y) {
+  return x * y ;
+}
+
+function dev(x, y) {
+  return x / y ;
+}
+
+function inc() 
